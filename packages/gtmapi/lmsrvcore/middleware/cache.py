feb2dbeadb44e8f6283c6f569aa5c86dc5eea06b
Bill Van Besien <bill@gigantum.io>
2019-08-14 17:29:03 -0400

    More commentary
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- packages/gtmapi/lmsrvcore/middleware/cache.py
+++ packages/gtmapi/lmsrvcore/middleware/cache.py
@@ -52,16 +52,16 @@ class RepositoryCacheMiddleware:
         return return_value
 
     def flush_repo_cache(self, operation_obj, variable_values: Dict) -> None:
-        """ Infers and extracts a repository (Labbook/Dataset) owner and name field from a given
-        mutation. Note that there are somewhat inconsistent namings in certain Mutation inputs,
-        so this method uses a variety of methods to capture it.
+        """ Clears the cache for ANY labbook or dataset that has attributes matching
+        any input field. This method will likely overestimate and remove too many
+        cache items, but we accept the cost as it will lead to fewer potential problems.
 
         Input:
             operation_obj: Reference to the actual Graphene GraphQL mutation
             variable_values: Dict containing the mutation Input fields
 
         Returns:
-            Tuple indicating username, owner, and repo name
+            None
         """
         input_vals = variable_values.get('input')
         if input_vals is None:
@@ -71,10 +71,10 @@ class RepositoryCacheMiddleware:
         if operation_obj.name.value in self.skip_mutations:
             raise SkipRepo(f"Skip mutation {operation_obj.name}")
 
-        owners = [ow for ow in (input_vals.get('owner'),
-                                input_vals.get('labbook_owner'),
-                                input_vals.get('dataset_owner'))
-                  if ow]
+        owners = [owner for owner in (input_vals.get('owner'),
+                                      input_vals.get('labbook_owner'),
+                                      input_vals.get('dataset_owner'))
+                  if owner]
         if not owners:
             raise UnknownRepo("No repository owner detected")
 
@@ -85,6 +85,13 @@ class RepositoryCacheMiddleware:
         if not names:
             raise UnknownRepo("No repository name detected")
 
+        # Here we go through and delete any and all cache entries for labbooks
+        # or datasets that match the input attributes. It in some cases may be
+        # a bit too aggressive in deleting a single cache entry, but we consider
+        # it worth the extremely minor cost to refetch a single repo.
+        #
+        # It must continue to be this way until we have a more structured way to
+        # discriminate whether the given mutation is for a Dataset or Labbook.
         lb_cache, ds_cache = LabbookCacheController(), DatasetCacheController()
         for owner, name in product(owners, names):
             lb_cache.clear_entry((get_logged_in_username(), owner, name))
