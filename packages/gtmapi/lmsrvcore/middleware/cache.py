6b482029ae804fef4110ca3716a3b0fcb6c5f12e
Bill Van Besien <bill@gigantum.io>
2019-08-14 14:48:18 -0400

    Attempt to aggressively clear cache
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- packages/gtmapi/lmsrvcore/middleware/cache.py
+++ packages/gtmapi/lmsrvcore/middleware/cache.py
@@ -1,4 +1,5 @@
 from typing import Dict, Tuple
+from itertools import product
 
 from gtmcore.logging import LMLogger
 from lmsrvcore.auth.user import get_logged_in_username
@@ -39,9 +40,7 @@ class RepositoryCacheMiddleware:
 
         if info.operation.operation == 'mutation':
             try:
-                repo_type, username, owner, name = self.parse_mutation(info.operation, info.variable_values)
-                cache_control = DatasetCacheController if repo_type == 'dataset' else LabbookCacheController
-                cache_control.build().clear_entry((username, owner, name))
+                self.flush_repo_cache(info.operation, info.variable_values)
             except UnknownRepo as e:
                 logger.warning(f'Mutation {info.operation.name} not associated with a repo: {e}')
             except SkipRepo:
@@ -52,7 +51,7 @@ class RepositoryCacheMiddleware:
         return_value = next(root, info, **args)
         return return_value
 
-    def parse_mutation(self, operation_obj, variable_values: Dict) -> Tuple[str, str, str, str]:
+    def flush_repo_cache(self, operation_obj, variable_values: Dict) -> None:
         """ Infers and extracts a repository (Labbook/Dataset) owner and name field from a given
         mutation. Note that there are somewhat inconsistent namings in certain Mutation inputs,
         so this method uses a variety of methods to capture it.
@@ -72,29 +71,21 @@ class RepositoryCacheMiddleware:
         if operation_obj.name.value in self.skip_mutations:
             raise SkipRepo(f"Skip mutation {operation_obj.name}")
 
-        repo_type = 'labbook'
-        owner = input_vals.get('owner')
-        if not owner:
-            owner = input_vals.get('labbook_owner')
-        # TODO! Include support for datasets.
-        if not owner:
-            owner = input_vals.get('dataset_owner')
-            repo_type = 'dataset'
-
-        if owner is None:
+        owners = [ow for ow in (input_vals.get('owner'),
+                                input_vals.get('labbook_owner'),
+                                input_vals.get('dataset_owner'))
+                  if ow]
+        if not owners:
             raise UnknownRepo("No repository owner detected")
 
-        repo_name = input_vals.get('name')
-        if not repo_name:
-            repo_name = input_vals.get('labbook_name')
-        # TODO! Include support for datasets.
-        if not repo_name:
-            repo_name = input_vals.get('dataset_name')
-            repo_type = 'dataset'
-
-        if repo_name is None:
+        names = [name for name in (input_vals.get('name'),
+                                   input_vals.get('labbook_name'),
+                                   input_vals.get('dataset_name'))
+                 if name]
+        if not names:
             raise UnknownRepo("No repository name detected")
 
-        return repo_type, get_logged_in_username(), owner, repo_name
-
-
+        lb_cache, ds_cache = LabbookCacheController.build(), DatasetCacheController.build()
+        for owner, name in product(owners, names):
+            lb_cache.clear_entry((get_logged_in_username(), owner, name))
+            ds_cache.clear_entry((get_logged_in_username(), owner, name))
